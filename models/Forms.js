cf5aa6958de81dc3a289deab21f4cb8c28e55154
michawei <freda0330@yahoo.com.tw>
2015-06-09 11:24:27 +0800

    Schema done

--- models/Forms.js
+++ models/Forms.js
@@ -1,10 +1,10 @@
 var mongoose = require('mongoose');
 
 var FormsSchema = new mongoose.Schema({
-	name: String,
-	completed: Boolean,
-	note: String,
-	updated_at: { type: Date, default: Date.now },
+	name: { type: String, unique: true },
+	category: String,
+	form: Array,
+	date: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model('Forms', FormsSchema);
